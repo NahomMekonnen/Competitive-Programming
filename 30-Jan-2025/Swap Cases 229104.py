ce6d8c752679395abe813e949b0396385cb3c93f
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-01-31 00:05:16 +0300

    Created 'Swap Cases' by A2SV Hub

---
+++ 30-Jan-2025/Swap Cases 229104.py
@@ -0,0 +1,11 @@
+# Problem: Swap Cases - https://www.hackerrank.com/challenges/swap-case?isFullScreen=true
+
+def swap_case(s):
+    ans = ""
+    for c in s:
+        if c.islower():
+            c = c.upper()
+        else:
+            c = c.lower()
+        ans += c
+    return ans
