622c7f075cd2e38a20ed9b056ee10d63e9647efe
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-01 09:07:57 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 01-Feb-2025/Love Story 233158.py
@@ -0,0 +1,11 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t  = int(input())
+base = "codeforces"
+for i in range(t):
+    word = input()
+    count = 0
+    for i in range(10):
+        if word[i] != base[i]:
+            count += 1
+    print(count)
