fc1964de32a9eb21664f481faf8d55f54f0645ba
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-03 23:36:26 +0300

    Created 'Team' by A2SV Hub

---
+++ 03-Feb-2025/Team 237720.py
@@ -0,0 +1,9 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n = int(input())
+count = 0
+for i in range(n):
+    a,b,c = map(int,input().split())
+    if (a + b + c) >= 2:
+        count +=1
+print(count)
