00c653f18bd35774cabeeef9aae64cfa4a2ddbc3
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-04 22:27:07 +0300

    Created 'Presents' by A2SV Hub

---
+++ 04-Feb-2025/Presents 241305.py
@@ -0,0 +1,6 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+n = int(input())
+friends  = list(map(int,input().split()))
+ans = [friends.index(i+1) + 1 for i in range(len(friends))]
+print(" ".join(list(map(str,ans))))
