c023f4676bfe3af715ddd369d5a381e99d7c591a
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-14 22:56:08 +0300

    Created 'Rabbits in Forest' by A2SV Hub

---
+++ 14-Feb-2025/Rabbits in Forest 266726.py
@@ -0,0 +1,13 @@
+# Problem: Rabbits in Forest - https://leetcode.com/problems/rabbits-in-forest/
+
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        bam = Counter(answers)
+        ans = 0
+        if 0 in bam: 
+            ans += bam[0]
+            del bam[0]
+        for i in bam :
+            m, n = i + 1, bam[i]
+            ans += int(math.ceil(n/m) * m)
+        return ans 
