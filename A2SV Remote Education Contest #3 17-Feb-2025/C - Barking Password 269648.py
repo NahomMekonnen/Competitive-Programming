3897779a4e5da7b53a08a1893f38accbf789afa0
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-17 22:03:04 +0300

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 269648.py
@@ -0,0 +1,17 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+pswrd = input()
+found = False
+first, second = [], []
+for i in range(int(input())) :
+    wrd = input()
+    first.append(wrd[0])
+    second.append(wrd[1])
+    if wrd == pswrd :
+        found = True
+for s in second :
+    for f in first :
+        if (s + f) == pswrd :
+            found = True
+            break
+print("YES" if found else  "NO")
