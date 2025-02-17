c8d0ba552a00639c8287b7b727d1066cc604a417
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-17 23:11:25 +0300

    Created 'D - Mugisho and Rufeyda' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 17-Feb-2025/D - Mugisho and Rufeyda 269874.py
@@ -0,0 +1,15 @@
+# Problem: D - Mugisho and Rufeyda - https://codeforces.com/gym/586622/problem/D
+
+inputs = list(map(int,input().split()))
+n , t = inputs[0], inputs[1]
+limit = 10 **n 
+found = False
+
+for i in range(limit//10,limit):
+    if (i % t) == 0 :
+        print(i)
+        found = True
+        break
+if not found:
+    print(-1)
+
