b016f10d0d115da717b10e01d3965b4229e94523
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-17 22:02:48 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/B - Card Game 269647.py
@@ -0,0 +1,17 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+from collections import defaultdict
+from collections import deque
+
+n = int(input())
+a = list(map(int,input().split()))
+b = defaultdict(deque)
+for i in range(n) :
+    b[a[i]].appendleft(i+1)
+a.sort()
+i, j =0, n-1
+while(i<j) :
+    print(f"{b[a[i]].pop()} {b[a[j]].pop()}")
+    i+=1
+    j-=1
+
