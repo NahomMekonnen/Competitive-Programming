67c7aa20337e134991abd2ca3090900ada2c2484
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-17 23:11:13 +0300

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 17-Feb-2025/C - Sura loves coding 269871.py
@@ -0,0 +1,16 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+n = int(input())
+s = input()
+left, middle,right= [],s[0],''
+for i in range(1,n) :
+    if i % 2 ==0 :
+        right += s[i]
+    else :
+        left.append(s[i])
+left.reverse()
+left = "".join(left)
+ans = list( left+middle + right)
+if n % 2 ==0:
+    ans.reverse()
+print("".join(ans))
