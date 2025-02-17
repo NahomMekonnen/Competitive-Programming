f4903b1b89e76158d9e406cda3e352bd6ce6517b
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-17 23:11:06 +0300

    Created 'B - The special paintbrush' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 17-Feb-2025/B - The special paintbrush 269868.py
@@ -0,0 +1,14 @@
+# Problem: B - The special paintbrush - https://codeforces.com/gym/586622/problem/B
+
+t  = int(input())
+for i in range(t):
+    n = int(input())
+    s = input()
+    i, j =0, n-1
+    while(s[i]!="B"):
+        i += 1
+    while(s[j]!="B"):
+        j -= 1
+    j+=1
+    print(j-i)
+        
