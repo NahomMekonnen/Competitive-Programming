8e17a6d3467cdc591a8801d05fdebe5d5ac1158e
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-17 22:03:25 +0300

    Created 'E - Zombie Invasion!' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/E - Zombie Invasion! 269652.py
@@ -0,0 +1,26 @@
+# Problem: E - Zombie Invasion! - https://codeforces.com/gym/588094/problem/E
+
+from collections import defaultdict
+
+for _ in range(int(input())) :
+    n , k = map(int,input().split())
+    heal = list(map(int,input().split()))
+    pos =list(map(int,input().split()))
+
+    zombs = defaultdict(int)
+    for i in range(len(pos)):
+        zombs[abs(pos[i])] += heal[i]
+    positions = sorted(zombs.keys())
+    alive, prev, carry = True, 0, 0
+    
+    for i in range(len(positions)) :
+        damage = (k * (positions[i] - prev)) + carry
+        if damage < zombs[positions[i]] :
+            alive = False
+            break
+        carry = damage - zombs[positions[i]]
+        prev = positions[i]
+
+    print("YES" if alive else "NO")
+    
+    # 1 2 3
