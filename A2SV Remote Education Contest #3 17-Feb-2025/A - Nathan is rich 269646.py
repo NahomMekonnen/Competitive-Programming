a1e32325f047b4d4969a852b222a12e02f2a8c7f
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-17 22:02:39 +0300

    Created 'A - Nathan is rich' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/A - Nathan is rich 269646.py
@@ -0,0 +1,11 @@
+# Problem: A - Nathan is rich - https://codeforces.com/gym/588094/problem/A
+
+t = int(input())
+for i in range(t) :
+    n = int(input())
+    ans = 0
+    while n % 4 != 0:
+        ans += 1
+        n -= 2
+    ans += n//4
+    print(ans)
