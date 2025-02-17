b30dde1dc74f5fabb4733bd00f08b9f0b2f41cd8
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-17 22:03:15 +0300

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/D - Adjacents, ewww ! 269650.py
@@ -0,0 +1,21 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+for _ in range(int(input())) :
+    n = int(input())
+    limit = n **2
+    if n == 1: 
+        print(1)
+    elif n == 2 :
+        print(-1)
+    else:
+        nums = []
+        for i in range(1,limit + 1) :
+            if i % 2 == 1:
+                nums.append(i)
+        for i in range(1,limit + 1) :
+            if i % 2 == 0:
+                nums.append(i)
+        j = 0
+        for i in range(n) :
+            print(" ".join(map(str,nums[j:j+n])))
+            j += n
