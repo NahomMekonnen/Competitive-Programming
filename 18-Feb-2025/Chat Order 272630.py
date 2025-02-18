4bb7b9b77857cc494376f94f56654a94b47d6b49
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-18 23:43:22 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 18-Feb-2025/Chat Order 272630.py
@@ -0,0 +1,10 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+time, messages = 0, dict()
+for _ in range(int(input())) :
+    message = input()
+    time+=1
+    messages[message]=time
+ans = dict(sorted(messages.items(),key=lambda x : x[1], reverse= True))
+for i in ans :
+    print(i)
