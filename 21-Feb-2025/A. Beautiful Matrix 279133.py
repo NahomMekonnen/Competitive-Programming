b54bcb71ccc81e720cb33719227a2c9b07582fa6
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-21 20:21:04 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 21-Feb-2025/A. Beautiful Matrix 279133.py
@@ -0,0 +1,16 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+array = [
+    list(map(int,input().split())),
+    list(map(int,input().split())),
+    list(map(int,input().split())),
+    list(map(int,input().split())),
+    list(map(int,input().split()))
+]
+indices = []
+for i in range(len(array)) :
+    for j in range(len(array)) :
+        if array[i][j] == 1:
+            indices = [i,j]
+            break
+print(abs(indices[0]-2)+abs(indices[1]-2))
