9f113f5ea74ca93bfaa981d15f626ffff57d907b
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-24 15:14:25 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 24-Feb-2025/Fox And Snake 281611.py
@@ -0,0 +1,18 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+row, col  = map(int,input().split())
+snake = []
+for i in range(row) :
+    if i % 2 == 0 :
+        snake.append(["#"]*col)
+    else :
+        snake.append(["."]*col)
+step = True
+for i in range(1,row,2) :
+    if step :
+        snake[i][-1] = "#"
+    else :
+        snake[i][0] = "#"
+    step = not step
+for i in snake :
+    print("".join(i))
