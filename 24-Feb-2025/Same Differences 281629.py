2cf3a413e7f61e64c0048ff45523643479ec0c79
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-02-24 15:43:09 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 24-Feb-2025/Same Differences 281629.py
@@ -0,0 +1,15 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+from collections import defaultdict
+
+
+for _ in range(int(input())):
+    n  = int(input())
+    a = list(map(int,input().split()))
+    hashmap = defaultdict(int)
+    cnt = 0 
+    for i in range(n) :
+        x = (a[i] - i)
+        cnt += hashmap[x]
+        hashmap[x] += 1   
+    print(cnt)
