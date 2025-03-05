fb9bfe135fd00d8d6c78d7c07e8e6e4a44176ae5
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-03-05 22:01:50 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 05-Mar-2025/Less or Equal 293532.py
@@ -0,0 +1,18 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+from collections import Counter
+n, k = map(int,input().split())
+
+a = sorted(list(map(int,input().split())))
+ans = a[0]-1 if k == 0 else a[k-1]
+cnt = 0 
+for i in a :
+    if i > ans :
+        break
+    cnt += 1
+
+if ans < 1 or cnt != k :
+    print(-1)
+else :
+    print(ans)
+
