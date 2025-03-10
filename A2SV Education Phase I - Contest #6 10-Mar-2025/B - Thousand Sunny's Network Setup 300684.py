6d6d93fff2406a944e56885bb617fadf9b71db0a
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-03-10 21:41:39 +0300

    Created 'B - Thousand Sunny's Network Setup' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/B - Thousand Sunny's Network Setup 300684.py
@@ -0,0 +1,15 @@
+# Problem: B - Thousand Sunny's Network Setup - https://codeforces.com/gym/594356/problem/B
+
+from collections import Counter
+
+n, k = map(int,input().split())
+a = list(map(int,input().split()))
+count= Counter(a)
+a = sorted(list(set(a)), reverse=True)
+ans , curr  =0, 0
+for i in range(len(a)):
+    curr += count[a[i]]
+    if curr >= k: 
+        ans = a[i]
+        break
+print(ans)
