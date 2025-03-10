962af6d08d591f3c310541ee04d389aa427b521a
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-03-10 21:41:11 +0300

    Created 'A - Zoro’s Bounty Dilemma' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/A - Zoro’s Bounty Dilemma 300682.py
@@ -0,0 +1,12 @@
+# Problem: A - Zoro’s Bounty Dilemma - https://codeforces.com/gym/594356/problem/A
+
+for _ in range(int(input())) :
+    s = set(input())
+    ans = 0
+    for i in s:
+        if i == "<" :
+            ans += 1
+        if i  == ">" :
+            ans += 2
+    print("=" if ans == 0 else "<" if ans == 1 else ">" if ans == 2 else "?")
+ 
