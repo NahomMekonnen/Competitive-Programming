44e892f222c9377bb5dbf8689673f217e9441107
Nahom <106447363+NahomMekonnen@users.noreply.github.com>
2025-03-10 21:41:27 +0300

    Created 'C - Robin’s Water Wisdom: Stop the Leaks!' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/C - Robin’s Water Wisdom: Stop the Leaks! 300683.py
@@ -0,0 +1,14 @@
+# Problem: C - Robin’s Water Wisdom: Stop the Leaks! - https://codeforces.com/gym/594356/problem/C
+
+n, A, B = map(int,input().split())
+s = list(map(int,input().split()))
+S, ans = sum(s), 0
+firstHole = s[0]
+s = s[1:]
+s.sort(reverse=True)
+i = 0
+while ((firstHole/S)*A) < B and (i < len(s)):
+    S -= s[i]
+    ans += 1
+    i+=1
+print(ans)
