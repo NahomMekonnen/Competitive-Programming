79b9cf237f03e98749a05df6cdcfbf5707ed1d40
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2025-10-16 23:42:50 +0300

    Implement nextGreaterElement function in Python

---
+++ Next Greater Element I.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [-1] * len(nums2)
+        stk = []
+        for i in range(len(nums2) -1, -1, -1) :
+            while stk and stk[-1] <= nums2[i] :
+                stk.pop()
+            if stk :
+                res[i] = stk[-1]
+            stk.append(nums2[i])
+        map = dict(zip(nums2,res))
+        return [map[i] for i in nums1]
+        
