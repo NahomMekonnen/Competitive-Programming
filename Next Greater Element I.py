26068a3bc1407a2961e7a8abeeab450104bb3767
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2025-10-17 00:02:26 +0300

    Fix index handling in nextGreaterElement function

--- Next Greater Element I.py
+++ Next Greater Element I.py
@@ -3,11 +3,10 @@ class Solution:
         res = [-1] * len(nums2)
         stk = []
         for i in range(len(nums2) -1, -1, -1) :
-            while stk and stk[-1] <= nums2[i] :
+            while stk and nums2[stk[-1]] <= nums2[i] :
                 stk.pop()
             if stk :
-                res[i] = stk[-1]
-            stk.append(nums2[i])
+                res[i] = nums2[stk[-1]]
+            stk.append(i)
         map = dict(zip(nums2,res))
         return [map[i] for i in nums1]
-        
