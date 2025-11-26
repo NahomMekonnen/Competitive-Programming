3942a5e718bf116186d5efe3ee6e1acb1b4a44f4
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2025-11-26 22:50:52 +0300

    Refactor prefixesDivBy5 for efficiency

--- Binary Prefix Divisible by 5.py
+++ Binary Prefix Divisible by 5.py
@@ -1,8 +1,7 @@
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
-        ans = []
-        nums = [str(i) for i in nums]
-        for i in range (len(nums)) : 
-            num = int("".join(nums[:i+1]),2)
-            ans.append(True if num % 5 == 0 else False)   
+        ans, num = [], ''
+        for i in nums :
+            num += str(i)
+            ans.append(int(num,2) % 5 == 0)
         return ans
