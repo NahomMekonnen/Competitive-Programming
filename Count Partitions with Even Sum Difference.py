a9b095f063028f8710670372590f81e5dd99243a
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2025-12-05 23:53:18 +0300

    Create Count Partitions with Even Sum Difference.py

---
+++ Count Partitions with Even Sum Difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPartitions(self, nums: List[int]) -> int:
+            n = len(nums)
+            for i in range(1,n) :
+                nums[i] += nums[i - 1]
+            cnt = 0
+            for i in range(n-1) :
+                diff = nums[i] - (nums[n-1] - nums[i])
+                if diff % 2 == 0 :
+                    cnt += 1
+            return cnt
