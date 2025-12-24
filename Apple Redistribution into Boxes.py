b09b81982cd297ac38e615d63d8d249e62915dba
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2025-12-24 23:58:25 +0300

    Update Apple Redistribution into Boxes.py

--- Apple Redistribution into Boxes.py
+++ Apple Redistribution into Boxes.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+        capacity.sort(reverse = True)
+        n = sum(apple)
+        ans = 0
+        for i in capacity :
+            n -= i
+            ans += 1
+            if n <= 0 :
+                break
+        return ans
