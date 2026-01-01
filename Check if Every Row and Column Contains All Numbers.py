c5f8cc7153d128c92ecec557dffc8b999a97a5b7
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-01-01 19:50:56 +0300

    Update Check if Every Row and Column Contains All Numbers.py

--- Check if Every Row and Column Contains All Numbers.py
+++ Check if Every Row and Column Contains All Numbers.py
@@ -1,13 +1,24 @@
+# class Solution:
+#     def checkValid(self, matrix: List[List[int]]) -> bool:
+#         rows, cols = defaultdict(set), defaultdict(set)
+#         for i in range(len(matrix)) :
+#             for j in range(len(matrix[i])) :
+#                 rows[i+1].add(matrix[i][j])
+#                 cols[j+1].add(matrix[i][j])
+#         for i in range(1,len(matrix)+1) :
+#             if len(rows[i]) != len(matrix) :
+#                 return False
+#             if len(cols[i]) != len(matrix) :
+#                 return False
+#         return True
+
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
-        rows, cols = defaultdict(set), defaultdict(set)
-        for i in range(len(matrix)) :
-            for j in range(len(matrix[i])) :
-                rows[i+1].add(matrix[i][j])
-                cols[j+1].add(matrix[i][j])
-        for i in range(1,len(matrix)+1) :
-            if len(rows[i]) != len(matrix) :
+        n = len(matrix)
+        for row in matrix :
+            if len(set(row)) != n :
                 return False
-            if len(cols[i]) != len(matrix) :
+        for col in zip(*matrix) :
+            if len(set(col)) != n :
                 return False
         return True
