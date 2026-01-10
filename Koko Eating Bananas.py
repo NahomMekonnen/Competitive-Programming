f8d18042699abcc6e5be053361527be63b96b92c
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-01-11 00:19:55 +0300

    Implement binary search for minimum eating speed

--- Koko Eating Bananas.py
+++ Koko Eating Bananas.py
@@ -1,1 +1,19 @@
 
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        ans =  r
+
+        while l <= r :
+            mid = l + ( r - l ) // 2
+
+            totalTime = 0
+            for i in piles :
+                totalTime += math.ceil(float(i)/mid)
+            
+            if totalTime <= h :
+                ans = mid 
+                r = mid - 1
+            else :
+                l = mid + 1
+        return ans
