46e251124a49369303838f1cf36efa82dbf705d9
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-01-17 00:29:48 +0300

    Update Maximum Subarray Sum with One Deletion.py

--- Maximum Subarray Sum with One Deletion.py
+++ Maximum Subarray Sum with One Deletion.py
@@ -1,1 +1,10 @@
+class Solution:
+    def maximumSum(self, arr: List[int]) -> int:
+        max_sum = cur_sum_del =  cur_sum = arr[0]
+        for i in arr[1:]:
+            cur_sum_del = max(cur_sum_del + i, i, cur_sum)
+            cur_sum = max(cur_sum + i, i)
+            max_sum = max(max_sum, cur_sum, cur_sum_del)
+        return max_sum
 
+        
