9f22b8712959ee89985230797027da64c1ea489d
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-02-27 22:51:21 +0300

    Create Sqrt(x).py

---
+++ Sqrt(x).py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if (x == 0 or x == 1) :
+            return x
+
+        start ,end = 1, x
+
+        while (start <= end) :
+            mid = start + (end - start)// 2
+
+            if (mid == x // mid) :
+                return mid
+            elif (mid < x // mid) :
+                start = mid + 1
+            else :
+                end = mid - 1
+
+        return end
