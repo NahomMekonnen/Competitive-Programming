228cebc047613a685428ce2c4375b4a235abfab7
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-03-05 23:59:47 +0300

    Update Majority Element II 260470.py

--- 12-Feb-2025/Majority Element II 260470.py
+++ 12-Feb-2025/Majority Element II 260470.py
@@ -1,7 +1,11 @@
-# Problem: Majority Element II - https://leetcode.com/problems/majority-element-ii/?envType=daily-question&envId=2023-10-05
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count = Counter(nums)
-        return [i for i in count if count[i] > (len(nums)//3)]
+        n=len(nums)//3
+        count=Counter(nums)
+        output=[]
+        for i in count:
+            if count[i]>n:
+                output.append(i)
+        return output
+        
         
