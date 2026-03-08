7cbcd06809cfcbe0a1bd94f83d76bb50b7c068a8
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-03-09 00:02:09 +0300

    Update Maximize Sum of At Most K Distinct Elements.py

--- Maximize Sum of At Most K Distinct Elements.py
+++ Maximize Sum of At Most K Distinct Elements.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def maxKDistinct(self, nums: List[int], k: int) -> List[int]:
+        nums = sorted(list(set(nums)), reverse=True)
+        if len(nums) < k :
+            return nums
+        return nums[:k]
