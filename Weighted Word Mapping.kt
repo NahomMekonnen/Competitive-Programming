09b5083fd20db32fa7c522a15c79685fd59b05d0
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-03-09 18:06:38 +0300

    Update Weighted Word Mapping.kt

--- Weighted Word Mapping.kt
+++ Weighted Word Mapping.kt
@@ -3,8 +3,11 @@ class Solution {
         val first = 'a'.code
         return buildString {
             for (word in words) {
-                val asciiBytes = word.toByteArray(charset = Charsets.US_ASCII).map { weights[it - first] }
-                val total = 25 - (asciiBytes.sum() % 26) + first
+                var sumWeights = 0
+                for (i in word)  {
+                    sumWeights += weights[i.code - first]
+                }
+                val total = 25-  (sumWeights % 26) + first
                 append(total.toChar())
             }
         }
