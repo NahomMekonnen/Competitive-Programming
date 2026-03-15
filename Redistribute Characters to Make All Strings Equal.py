43eaac8744c150334c342991c8224bb9140aa91b
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-03-15 23:57:14 +0300

    Update Redistribute Characters to Make All Strings Equal.py

--- Redistribute Characters to Make All Strings Equal.py
+++ Redistribute Characters to Make All Strings Equal.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        c = Counter("".join(words))
+        print(c)
+        for i in c :
+            if c[i] % len(words) != 0 :
+                return False
+        return True
