b59c60ed8b7c4f438b7e7582a7df62d5c91de4d7
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-04-14 00:02:33 +0300

    Update Lucky Numbers in a Matrix.py

--- Lucky Numbers in a Matrix.py
+++ Lucky Numbers in a Matrix.py
@@ -1,1 +1,14 @@
+class Solution:
+    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
+        ans = []
+        m = len(matrix)
+        n = len(matrix[0])
+        possible = [min(i) for i in matrix]
+        for i in range(n) :
+            l = 0
+            for j in range(m) :
+                l = max(l, matrix[j][i])
+            if l in possible :
+                ans.append(l)
 
+        return ans 
