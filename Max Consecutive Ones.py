718ca89bf68af09ea4bec31acde74a99bce1f884
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-05-07 00:13:47 +0300

    Implement method to find max consecutive ones

--- Max Consecutive Ones.py
+++ Max Consecutive Ones.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        nums.append(0)
+        last = -1
+        ans = 0
+        for i in range(len(nums)) :
+            if nums[i] == 0 :
+                ans = max(ans, i - last - 1)
+                last = i
+        if last == -1 :
+            return len(nums)
+        return ans
