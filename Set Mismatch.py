b1af632a92eaabd69a149dde759210b403772403
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-05-08 22:56:51 +0300

    Refactor findErrorNums to simplify error detection

--- Set Mismatch.py
+++ Set Mismatch.py
@@ -1,11 +1,10 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        cnt = Counter(nums) 
-        ans = []
-        for i in cnt :
+        cnt = Counter(nums)
+        ans = [0,0]
+        for i in range(1, len(nums) + 1) :
             if cnt[i] > 1 :
-                ans.append(i)
-        for i in range(1,len(nums)+1) :
-            if cnt[i] == 0 :
-                ans.append(i)
+                ans[0] = i
+            elif cnt[i] == 0 :
+                ans[1] = i
         return ans
