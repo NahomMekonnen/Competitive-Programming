910162042c49d510f70c7664685358f2599cbc0e
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-06-03 00:43:19 +0300

    Update Earliest Finish Time for Land and Water Rides I.py

--- Earliest Finish Time for Land and Water Rides I.py
+++ Earliest Finish Time for Land and Water Rides I.py
@@ -1,1 +1,14 @@
+class Solution:
+    def earliestFinishTime(self, lst: List[int], ld: List[int], wst: List[int], wd: List[int]) -> int:
+        ans = float('Inf')
 
+        for i in range(len(lst)):
+            lf = lst[i] + ld[i]
+            for j in range(len(wst)) :
+                ans = min(ans, max(lf, wst[j]) + wd[j])
+
+        for i in range(len(wst)):
+            wf = wst[i] + wd[i]
+            for j in range(len(lst)) :
+                ans = min(ans, max(wf, lst[j]) + ld[j])
+        return ans
