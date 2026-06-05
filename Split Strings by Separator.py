2d608a5c091d5c8be62758b2964fd2b7ab94d24f
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-06-05 23:59:24 +0300

    Update Split Strings by Separator.py

--- Split Strings by Separator.py
+++ Split Strings by Separator.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        word = ""
+        for i in words:
+            word += i + separator
+        split = word.split(separator)
+        return [i for i in split if i != ""]
