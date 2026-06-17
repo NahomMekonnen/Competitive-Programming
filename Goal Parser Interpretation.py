86d9f9cf94b79b2ed7f08fccea011d514b26b9ac
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-06-17 22:31:19 +0300

    Create Goal Parser Interpretation.py

---
+++ Goal Parser Interpretation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        ans = ""
+        for i in range(len(command)) :
+            if command[i] == "G" :
+                ans += command[i]
+            elif command[i] == "(" :
+                if command[i + 1] == ")" :
+                    ans += "o"
+                    i += 2
+                else :
+                    ans += "al"
+                    i += 4
+        return ans 
