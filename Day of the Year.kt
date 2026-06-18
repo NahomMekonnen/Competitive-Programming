9fd704d3adfc521e8e865695af60cdf3fc6c41f8
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-06-19 00:00:31 +0300

    Update Day of the Year.kt

--- Day of the Year.kt
+++ Day of the Year.kt
@@ -1,1 +1,7 @@
-
+import java.time.LocalDate
+class Solution {
+    fun dayOfYear(date: String): Int {
+        val d = date.split("-").map{ it.toInt()} 
+        return LocalDate.of(d[0],d[1],d[2]).dayOfYear
+    }
+}
