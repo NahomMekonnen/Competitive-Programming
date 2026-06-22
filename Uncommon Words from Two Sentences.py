c87dedd23a1004068e1dd1febf33f83d182b51a9
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-06-23 00:00:06 +0300

    Update Uncommon Words from Two Sentences.py

--- Uncommon Words from Two Sentences.py
+++ Uncommon Words from Two Sentences.py
@@ -1,1 +1,9 @@
 
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        words = defaultdict(int)
+        for i in s1.split(" ") :
+            words[i] +=1
+        for i in s2.split(" ") :
+            words[i] +=1
+        return [i for i in words if words[i] == 1]
