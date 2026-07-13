7e72911b424739bcf62429bb267e7ad1abcdddfa
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-07-14 00:06:12 +0300

    Update Thousand Separator.py

--- Thousand Separator.py
+++ Thousand Separator.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def thousandSeparator(self, n: int) -> str:
+        if n < 1000 :
+            return str(n)
+        ans = []
+        while n > 0 :
+            k = str(n % 1000)
+            n //= 1000
+            if n > 0 :
+                k = "00" + k
+                k = k[-3:]
+            ans.append(k)
+        
+        return ".".join(ans[::-1])
