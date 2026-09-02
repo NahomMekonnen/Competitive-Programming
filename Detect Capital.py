802345469bbe5f6db93d472da6209f9bc1c06e7c
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-09-02 23:53:56 +0300

    Implement capital usage detection in Solution class

---
+++ Detect Capital.py
@@ -0,0 +1,7 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.upper() == word :
+            return True
+        if word.lower() == word :
+            return True
+        return word == word[0].upper() + word[1:].lower()
