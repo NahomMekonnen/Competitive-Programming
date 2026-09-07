e1ba6084cac7d0a9f0cc9b4424db7af293c51d39
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-09-08 00:23:57 +0300

    Add hIndex method to Solution class
    Implement hIndex method to calculate H-Index from citations.

--- H-Index.py
+++ H-Index.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def hIndex(self, c: List[int]) -> int:
+        cnt = defaultdict(int)
+        c.sort(reverse = True)
+        h = 0
+        for i in range(len(c)) : 
+            gtoe = i + 1 # greater than or equal count
+            cnt[c[i]] = gtoe
+        for i in cnt :
+            if i != 0 :
+                if i <= cnt[i] :
+                    h = max(h, i)
+                else :
+                    h = max(h, cnt[i])
+        return  h
