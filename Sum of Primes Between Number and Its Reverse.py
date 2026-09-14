7f8abc689287212e0c0971add7db590f839e7a86
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-09-15 00:42:00 +0300

    Update Sum of Primes Between Number and Its Reverse.py

--- Sum of Primes Between Number and Its Reverse.py
+++ Sum of Primes Between Number and Its Reverse.py
@@ -2,15 +2,18 @@ class Solution:
     def sumOfPrimesInRange(self, n: int) -> int:
         r = int(str(n)[::-1])
         ans = 0
-        for i in range (min(n, r), max(n, r) + 1) :      
 
-
-            if i > 10 :
-                if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0 and i % 11 != 0 :
+        for i in range (min(n, r), max(n, r) + 1) :   
+            if i == 2 :
                 ans += i
-            else if i == 2 or i == 3 or i == 5 or i == 7 :
-                ans += i
-
-        
+            elif i > 2 and i % 2 != 0 :
+                root = int(sqrt(i))
+                has_factors = False
+                for f in range(3, root + 1, 2) :
+                    if i % f == 0 :
+                        has_factors = True
+                        break
+                if not has_factors :
+                    ans += i           
 
         return ans
