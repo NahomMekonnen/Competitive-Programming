a220da4411b5e3c8491bc8b6454c2d0dedeb2d1e
Nahom  Mekonnen <106447363+NahomMekonnen@users.noreply.github.com>
2026-09-27 00:04:52 +0300

    Solve Evaluate the Bracket Pairs of a String [74 ms / 52 MB]

--- Evaluate the Bracket Pairs of a String.py
+++ Evaluate the Bracket Pairs of a String.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
+        k = defaultdict(str)
+        for i in knowledge :
+            k[i[0]] = i[1]
+        ans = ""
+        i = 0
+        while i < len(s) :
+            if s[i] == "(" :
+                j = i + 1
+                while j < len(s) and s[j] != ")" :
+                    j += 1
+                if k[s[i + 1:j]] != "" :
+                    ans += k[s[i + 1:j]]
+                else :
+                    ans += "?"
+                i = j + 1
+            else :
+                ans += s[i] 
+                i += 1
+        return ans
